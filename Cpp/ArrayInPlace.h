1b495bb22626a15dab8216219ea3b5aa32729e03
Zhenyi He <snowymohehe@gmail.com>
2019-09-05 18:36:10 -0400

    Update ArrayInPlace.h
    Finish No.442

--- Cpp/ArrayInPlace.h
+++ Cpp/ArrayInPlace.h
@@ -298,4 +298,23 @@ namespace ArrayInPlace {
 		}
 		return ret;
 	    }
+	
+	// No.287. Find the Duplicate Number
+	// Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), 
+	// prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+	// You must not modify the array (assume the array is read only).
+	// You must use only constant, O(1) extra space.
+	// Your runtime complexity should be less than O(n2).
+	// There is only one duplicate number in the array, but it could be repeated more than once.
+	// mine
+	int findDuplicate(vector<int>& nums) {
+		int len = nums.size();
+		for(int i = 0; i < len; i++){
+		    for(int j = i+1; j < len; j++){
+			if(nums[i] == nums[j])
+			    return nums[i];
+		    }
+		}
+		return 0;
+	    }
 }
