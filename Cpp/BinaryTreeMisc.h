c55cd7010da7862a65aa24db569c8481111a67d6
Zhenyi He <snowymohehe@gmail.com>
2019-10-16 22:30:09 -0400

    Finish No.110

--- Cpp/BinaryTreeMisc.h
+++ Cpp/BinaryTreeMisc.h
@@ -510,6 +510,29 @@ class BinaryTreeMisc {
 		}
 		return depth;
 	    }
+	// No. 110 Balanced Binary Tree(Mine)
+	// Given a binary tree, determine if it is height-balanced.
+	// For this problem, a height-balanced binary tree is defined as:
+	// a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+	bool ret = true;
+	    int getDepth(TreeNode* root){
+		if(!root)
+		    return 0;
+
+		int leftDepth = 0, rightDepth = 0;
+		if(root->left)
+		    leftDepth = 1+ getDepth(root->left);
+		if(root->right)
+		    rightDepth = 1 + getDepth(root->right);
+		if(abs(leftDepth-rightDepth) > 1)
+		    ret = false;
+		return max(leftDepth, rightDepth);
+
+	    }
+	    bool isBalanced(TreeNode* root) {
+		getDepth(root);
+		return ret;
+	    }
 };
 
 
