a44a4420b486e159a87c2d1fbe7b2fb1e6ea8352
Zhenyi He <snowymohehe@gmail.com>
2019-10-18 03:12:01 -0400

    Finish No.700

--- Cpp/BinaryTreeMisc.h
+++ Cpp/BinaryTreeMisc.h
@@ -986,6 +986,19 @@ class BinaryTreeMisc {
 		inorder(root, temp);
 		return ret;   
 	}
+	// No.700 Search in a Binary Search Tree(Mine)
+	// Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+	TreeNode* searchBST(TreeNode* root, int val) {
+		if(!root)
+		    return NULL;
+		if(root->val == val)
+		    return root;
+		if(root->val < val)
+		    return searchBST(root->right, val);
+		if(root->val > val)
+		    return searchBST(root->left, val);
+		return NULL;
+	    }
 };
 
 
