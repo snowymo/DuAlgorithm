57bfd39446c0362c692a7046bcf1b878edc15542
Zhenyi He <snowymohehe@gmail.com>
2019-10-18 01:45:20 -0400

    Optimized No.329

--- Cpp/BinaryTreeMisc.h
+++ Cpp/BinaryTreeMisc.h
@@ -918,6 +918,26 @@ class BinaryTreeMisc {
 		}
 		return ret;
 	    }
+	// optimized, not necessary to create a tree, just fill the depth for each cell
+	int longestIncreasingPath(vector<vector<int>>& matrix) {
+		if(matrix.empty()) return 0;
+		const int r = matrix.size(), c = matrix[0].size();
+		vector<vector<int>> m(r, vector<int>(c, 0));
+		int count = 1;
+		for(int i = 0; i < r; ++i)
+		    for(int j = 0; j < c; ++j)
+			if(!m[i][j]) count = max(count, dfs(matrix, m, i, j));
+		return count;
+	    }
+	    static int dfs(vector<vector<int>>& matrix, vector<vector<int>>& m, const int i, const int j) {
+		if(m[i][j]) return m[i][j];
+		int r = matrix.size(), c = matrix[0].size();
+		if(i > 0 && matrix[i-1][j] > matrix[i][j]) m[i][j] = max(m[i][j], dfs(matrix, m, i-1, j));
+		if(i < r-1 && matrix[i+1][j] > matrix[i][j]) m[i][j] = max(m[i][j], dfs(matrix, m, i+1, j));
+		if(j > 0 && matrix[i][j-1] > matrix[i][j]) m[i][j] = max(m[i][j], dfs(matrix, m, i, j-1));
+		if(j < c-1 && matrix[i][j+1] > matrix[i][j]) m[i][j] = max(m[i][j], dfs(matrix, m, i, j+1));
+		return ++m[i][j];
+	    }
 };
 
 
