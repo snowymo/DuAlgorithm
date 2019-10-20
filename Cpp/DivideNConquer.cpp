9cbb0a13ba80214d8eecfc8905362b542034fb1d
Zhenyi He <snowymohehe@gmail.com>
2019-10-20 01:47:58 -0400

    Finish No.169

--- Cpp/DivideNConquer.cpp
+++ Cpp/DivideNConquer.cpp
@@ -1,5 +1,17 @@
 #pragma once
 #include "common.h"
 
-namespace DivideNConquer {
+namespace DivideNConquer {//Mine
+  // No.169 Majority Element
+  // Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+  // You may assume that the array is non-empty and the majority element always exist in the array.
+  int majorityElement(vector<int>& nums) {
+        int count = nums.size()/2;
+        map<int,int> mymap;
+        for(int i = 0; i < nums.size(); i++){
+           if(++mymap[nums[i]] > count)
+               return nums[i];
+        }
+        return 0;
+    }
 }
