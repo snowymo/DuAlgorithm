edaf34190c4aa975f5ab412ba0e95b5b697a2105
Zhenyi He <snowymohehe@gmail.com>
2019-10-20 01:49:59 -0400

    Finish No.53

--- Cpp/DivideNConquer.cpp
+++ Cpp/DivideNConquer.cpp
@@ -2,6 +2,19 @@
 #include "common.h"
 
 namespace DivideNConquer {//Mine
+  // No.53 Maximum Subarray
+  // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+  int maxSubArray(vector<int>& nums) {
+        int curMax = nums[0];
+        int allMax = curMax;
+        for(int i = 1; i < nums.size(); i++){
+            curMax = max(curMax+nums[i], nums[i]);
+            if(curMax > allMax)
+                allMax = curMax;
+        }
+        return allMax;
+    }
+  
   // No.169 Majority Element
   // Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
   // You may assume that the array is non-empty and the majority element always exist in the array.
