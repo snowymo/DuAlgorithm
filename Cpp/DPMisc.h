a6e39ea8f564c816288e7499c2383dfb987e094b
Zhenyi He <snowymohehe@gmail.com>
2019-10-24 17:36:19 -0400

    Finish No.1137

--- Cpp/DPMisc.h
+++ Cpp/DPMisc.h
@@ -120,4 +120,29 @@ class DPMisc{
         }
         return ans;
     }
+  
+  // No.1137 N-th Tribonacci Number(Mine)
+  // The Tribonacci sequence Tn is defined as follows: 
+  // T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+  // Given n, return the value of Tn.
+
+  // Example 1:
+  // Input: n = 4
+  // Output: 4
+  // Explanation:
+  // T_3 = 0 + 1 + 1 = 2
+  // T_4 = 1 + 1 + 2 = 4
+  int tribonacci(int n) {
+        if(n < 3)
+            return n > 0;
+        int memo0 = 0, memo1 = 1, memo2 = 1;
+        int ans;
+        for(int i = 3; i <= n; i++){
+            ans = memo0 + memo1 + memo2;
+            memo0 = memo1;
+            memo1 = memo2;
+            memo2 = ans;
+        }
+        return ans;
+    }
 }
