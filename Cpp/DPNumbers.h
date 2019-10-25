3c5d903136c50333982165cc8fe3b1ac026c9be7
Zhenyi He <snowymohehe@gmail.com>
2019-10-24 21:23:41 -0400

    Finish No. 446 after reading the solution

--- Cpp/DPNumbers.h
+++ Cpp/DPNumbers.h
@@ -72,4 +72,46 @@ public:
 		ans += (count+1) * count/2;
 		return ans;
 	    }
+	
+	// No.446 Arithmetic Slices II - Subsequence(Mine)
+	// A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+	// For example, these are arithmetic sequences:
+	// 1, 3, 5, 7, 9
+	// 7, 7, 7, 7
+	// 3, -1, -5, -9
+	// The following sequence is not arithmetic.
+	// 1, 1, 2, 5, 7
+
+	// A zero-indexed array A consisting of N numbers is given. A subsequence slice of that array is any sequence of integers (P0, P1, ..., Pk) such that 0 ≤ P0 < P1 < ... < Pk < N.
+	// A subsequence slice (P0, P1, ..., Pk) of array A is called arithmetic if the sequence A[P0], A[P1], ..., A[Pk-1], A[Pk] is arithmetic. In particular, this means that k ≥ 2.
+	// The function should return the number of arithmetic subsequence slices in the array A.
+	// The input contains N integers. Every integer is in the range of -231 and 231-1 and 0 ≤ N ≤ 1000. The output is guaranteed to be less than 231-1.
+	 // Example:
+	// Input: [2, 4, 6, 8, 10]
+	// Output: 7
+
+	// Explanation:
+	// All arithmetic subsequence slices are:
+	// [2,4,6]
+	// [4,6,8]
+	// [6,8,10]
+	// [2,4,6,8]
+	// [4,6,8,10]
+	// [2,4,6,8,10]
+	// [2,6,10]
+	int numberOfArithmeticSlices(vector<int>& A) {
+		vector<map<long long, int>> memos (A.size());
+		long long ans = 0;
+		for(int i = 1; i < A.size(); i++){
+		    for(int j = 0; j < i; j++){
+			long long diff = (long long)A[i]-(long long)A[j];
+			long long cur = 0;
+			if(memos[j].find(diff) != memos[j].end())
+			    cur = memos[j][diff];
+			memos[i][diff] += cur + 1;
+			ans += cur;
+		    }
+		}
+		return (int)ans;
+	    }
 };
