aab875c5caad059b28f44878a15559fb6ec6fd1d
Zhenyi He <snowymohehe@gmail.com>
2019-11-08 17:28:49 -0500

    Finish No.88

--- Cpp/SortMerge.h
+++ Cpp/SortMerge.h
@@ -169,4 +169,24 @@ namespace MergeSort {
 			nums1[k--] = nums2[j--];
 		}
 	}
+	// Mine
+	 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		vector<int> ans;
+		int i,j;
+		for( i = 0, j = 0; i < m && j < n; ){  
+		    if(nums1[i] < nums2[j]){
+			 ans.push_back(nums1[i++]);
+		    }
+		    else{
+		       ans.push_back(nums2[j++]);
+		    }       
+		}
+	       for(; i < m; i++ ){
+		   ans.push_back(nums1[i]);
+	       }
+		 for(; j < n; j++ ){
+		   ans.push_back(nums2[j]);
+	       }
+	    nums1 = ans;
+	    }
 }
