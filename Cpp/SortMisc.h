b334049b16235532037eb1960f9913f1aa67b5c9
Zhenyi He <snowymohehe@gmail.com>
2019-11-09 06:38:08 -0500

    Finish No.1122

--- Cpp/SortMisc.h
+++ Cpp/SortMisc.h
@@ -150,4 +150,59 @@ namespace SortMisc {
 		    return -1;
 		return 0;
 	    }
+	
+	// No.1122 Relative Sort Array
+	// Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+	// Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.  Elements that don't appear in arr2 should be placed at the end of arr1 in ascending order.
+	// Example 1:
+	// Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+	// Output: [2,2,2,1,4,3,3,9,6,7,19]
+
+	// Constraints:
+	// arr1.length, arr2.length <= 1000
+	// 0 <= arr1[i], arr2[i] <= 1000
+	// Each arr2[i] is distinct.
+	// Each arr2[i] is in arr1.
+	vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+		unordered_map<int,int> mymap;
+		for(int i = 0; i < arr2.size(); i++){
+		    mymap[arr2[i]] = 1;
+		}
+		vector<int> ret1, ret2;
+		ret1 = arr2;
+		for(int i = 0; i < arr1.size(); i++){
+		    if(mymap.find(arr1[i]) != mymap.end()){
+		      if(mymap[arr1[i]] == 1){
+			  mymap[arr1[i]] = 0;
+		      }  else{
+			auto p = find(ret1.begin(), ret1.end(), arr1[i]);
+			  ret1.insert(p, arr1[i]);
+		      }
+		    } else{
+			// insert into ret2 in sorted order
+			binaryInsert(arr1[i], ret2);
+		    }
+		}
+		// insert ret2 to ret1
+		//cout << ret2[0] << " " << ret2[1];
+		std::move(ret2.begin(), ret2.end(), std::back_inserter(ret1));
+		return ret1;
+	    }
+	    void binaryInsert(int element, vector<int>& ret2){
+		if(ret2.size() == 0){
+		    ret2.push_back(element);
+		    return;
+		}
+		int left = 0, right = ret2.size()-1;
+		while(left <= right){
+		    int mid = (left+right) >> 1;
+		    if(ret2[mid] < element){
+			left = mid+1;
+		    }else
+			right = mid-1;
+		}
+		ret2.insert(ret2.begin()+left, element);
+
+	    }
 }
