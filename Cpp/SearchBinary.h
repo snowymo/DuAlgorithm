81f4741da2d3424175a8b918cd6347ce86557da6
Zhenyi He <snowymohehe@gmail.com>
2019-11-13 16:04:25 -0500

    Finish No.719

--- Cpp/SearchBinary.h
+++ Cpp/SearchBinary.h
@@ -499,6 +499,26 @@ public:
 		}
 		return ans;
 	}
+	// Mine is TLE
+	// The solution
+	int smallestDistancePair(vector<int>& nums, int k) {
+		sort(nums.begin(), nums.end());
+
+		int lo = 0;
+		int hi = nums.back() - nums[0];
+		while (lo < hi) {
+		    int mi = (lo + hi) / 2;
+		    int count = 0, left = 0;
+		    for (int right = 0; right < nums.size(); ++right) {
+			while (nums[right] - nums[left] > mi) left++;
+			count += right - left;
+		    }
+		    //count = number of pairs with distance <= mi
+		    if (count >= k) hi = mi;
+		    else lo = mi + 1;
+		}
+		return lo;
+	    }
 
 	// 349. Intersection of Two Arrays
 	// Given two arrays, write a function to compute their intersection.
