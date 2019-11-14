05419769587fd1a880cabfb2f17d256b7ab206a6
Zhenyi He <snowymohehe@gmail.com>
2019-11-13 22:34:35 -0500

    Finish No.238

--- Cpp/Scanning.h
+++ Cpp/Scanning.h
@@ -551,6 +551,33 @@ class Scanning {
 		}
 		return l;
 	}
+	// Mine
+	vector<int> productExceptSelf(vector<int>& nums) {
+		vector<int> ret(nums.size(), 1);
+		vector<int> L(nums.size(), 1), R(nums.size(), 1);
+		for(int i = 1; i < nums.size(); i++){
+		    L[i] = L[i-1] * nums[i-1];
+		}
+		for(int i = nums.size()-2; i >= 0; i--){
+		    R[i] = R[i+1] * nums[i+1];
+		}
+		for(int i = 0; i < ret.size(); i++){
+		    ret[i] = L[i] * R[i];
+		}
+		return ret;
+	    }
+	vector<int> productExceptSelf(vector<int>& nums) {
+		vector<int> ret(nums.size(), 1);
+		for(int i = 1; i < nums.size(); i++){
+		    ret[i] = ret[i-1] * nums[i-1];
+		}
+		int R = 1;
+		for(int i = nums.size()-2; i >= 0; i--){
+		    R *= nums[i+1];
+		    ret[i] *= R;
+		}
+		return ret;
+	    }
 
 	// 274. H-Index
 	int hIndex(vector<int>& citations) {
