fcc00cc40fa287ba3f9e9ee235218fcf05847636
Zhenyi He <snowymohehe@gmail.com>
2019-11-13 19:46:07 -0500

    Finish No.11

--- Cpp/Scanning.h
+++ Cpp/Scanning.h
@@ -114,6 +114,19 @@ class Scanning {
 
 		return ans;
 	}
+	// Mine
+	int maxArea(vector<int>& height) {
+		int ret = 0;
+		for(int i = 0, j = height.size()-1; i < j;){
+		    ret = max(ret,min(height[i], height[j]) * (j-i));
+		    if(height[i] < height[j]){
+			++i;
+		    }else{
+			--j;
+		    }
+		}
+		return ret;
+	    }
 
 	// 53. Maximum Subarray
 	// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
