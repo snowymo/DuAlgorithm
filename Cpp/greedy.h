c68d6bdb78d4ee23c4d037237775d3c0520eae77
Zhenyi He <snowymohehe@gmail.com>
2019-11-21 01:56:37 -0500

    Finish No.1005

--- Cpp/greedy.h
+++ Cpp/greedy.h
@@ -349,4 +349,60 @@ namespace Greedy {
 		}
 		return true;
 	    }
+	
+	// No.1005 Maximize Sum Of Array After K Negations (Mine)
+	// Given an array A of integers, we must modify the array in the following way: we choose an i and replace A[i] with -A[i], and we repeat this process K times in total.  (We may choose the same index i multiple times.)
+
+	// Return the largest possible sum of the array after modifying it in this way.
+
+
+
+	// Example 1:
+
+	// Input: A = [4,2,3], K = 1
+	// Output: 5
+	// Explanation: Choose indices (1,) and A becomes [4,-2,3].
+	// Example 2:
+
+	// Input: A = [3,-1,0,2], K = 3
+	// Output: 6
+	// Explanation: Choose indices (1, 2, 2) and A becomes [3,1,0,2].
+	// Example 3:
+
+	// Input: A = [2,-3,-1,5,-4], K = 2
+	// Output: 13
+	// Explanation: Choose indices (1, 4) and A becomes [2,3,-1,5,4].
+
+
+	// Note:
+
+	// 1 <= A.length <= 10000
+	// 1 <= K <= 10000
+	// -100 <= A[i] <= 100
+	int largestSumAfterKNegations(vector<int>& A, int K) {
+		// replace all the negative numbers as many as we can, let us say we have N negative numbers
+		// if N <= K, then we replace (K-N)%2 (either 0 or 1) positive(or 0) numbers.
+		// if n > K, we stop
+
+		int smallest = INT_MAX;
+		int sum = 0;
+		sort(A.begin(), A.end());
+		auto lower = lower_bound(A.begin(), A.end(), 0);
+		int N = lower - A.begin();
+		if(K >= N){
+		    // flip all
+		    if((K-N)%2==0){
+			return abs(accumulate(A.begin(), lower, 0)) + accumulate(lower, A.end(), 0);
+		    }else{
+			// find the smallest absolute to substract
+			int toBeSub = A[N];
+			if(N>0 && abs(A[N-1]) < A[N])
+			    toBeSub = abs(A[N-1]);
+			return abs(accumulate(A.begin(), lower, 0)) - toBeSub*2 + accumulate(lower, A.end(), 0);
+		    }
+		}else{
+		    // flip K
+		    return abs(accumulate(A.begin(), A.begin()+K, 0)) + accumulate(A.begin()+K, A.end(), 0);
+		}
+	    }
 }
