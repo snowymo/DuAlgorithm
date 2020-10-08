c5f794e43d93d1c81218f9d6e7fa178c82a65101
Zhenyi He <snowymohehe@gmail.com>
2020-10-08 00:14:56 -0400

    Finish No.721 Merge accounts

--- Cpp/UnionSets.h
+++ Cpp/UnionSets.h
@@ -75,4 +75,73 @@ public:
 		}
 		return res;
 	}
+// 	Mine No.721 Accounts Merge[M]
+// 	Given a list accounts, each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+
+// Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some email that is common to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+
+// After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+// Example 1:
+// Input: 
+// accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+// Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],  ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+// Explanation: 
+// The first and third John's are the same person as they have the common email "johnsmith@mail.com".
+// The second John and Mary are different people as none of their email addresses are used by other accounts.
+// We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'], 
+// ['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+// Note:
+
+// The length of accounts will be in the range [1, 1000].
+// The length of accounts[i] will be in the range [1, 10].
+// The length of accounts[i][j] will be in the range [1, 30].
+	vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+		unordered_map<string, vector<string>> ccg;
+		unordered_map<string, int> email2name;
+		vector<vector<string>> result;
+	//         construct adj
+		for(int i = 0; i < accounts.size(); i++){
+		    // accounts[i][0] = "!" + accounts[i][0]; // to ensure this is the first one after sorting
+		    for(int j = 1; j < accounts[i].size()-1; j++){
+			ccg[accounts[i][j]].push_back(accounts[i][j+1]);
+			ccg[accounts[i][j+1]].push_back(accounts[i][j]);
+			email2name[accounts[i][j]] = i;
+			email2name[accounts[i][j+1]] = i;
+		    }
+		    if(accounts[i].size() == 2){
+			// only one email here
+			result.push_back(accounts[i]);
+		    }
+		}
+	//         connect ccg
+		unordered_map<string, bool> visited;
+		for(auto iter = ccg.begin(); iter != ccg.end(); ++iter){
+		    if(visited.find(iter->first) == visited.end()){
+			vector<string> curCCG;
+			connect(ccg, visited, iter->first, curCCG);
+
+			sort(curCCG.begin(), curCCG.end());
+			//                 email first
+			curCCG.insert(curCCG.begin(),accounts[email2name[iter->first]][0]);
+			result.push_back(curCCG);
+		    }
+		}
+		return result;
+	    }
+
+	    void connect(unordered_map<string, vector<string>>& ccg, unordered_map<string, bool>& visited,
+			 string email, vector<string>& curCCG){
+		visited[email] = true;        
+		curCCG.push_back(email);
+
+		if(ccg.find(email) == ccg.end())
+		    return;
+
+		for(auto iter = ccg[email].begin(); iter != ccg[email].end(); iter++){
+		    if(visited.find(*iter) == visited.end()){
+			connect(ccg, visited, *iter, curCCG);
+		    }
+		}
+	    }
 };
