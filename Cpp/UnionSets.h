17f1963ad97862cf409ed4f9c1806f901f91bbb1
Zhenyi He <snowymohehe@gmail.com>
2020-10-08 02:09:27 -0400

    Finished No.399 Evaluate Division[M]

--- Cpp/UnionSets.h
+++ Cpp/UnionSets.h
@@ -144,4 +144,144 @@ public:
 		    }
 		}
 	    }
+	
+// 	Mine No.399 Evaluate Division[M]
+// 	hint: union find with a value
+// 	You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+
+// You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+
+// Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+// Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+
+ 
+
+// Example 1:
+
+// Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+// Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+// Explanation: 
+// Given: a / b = 2.0, b / c = 3.0
+// queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+// return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+	void printTable(vector<vector<double>> & table){
+		for(int i = 0; i < table.size(); i++){
+		    for(int j = i+1; j < table.size(); j++){
+			cout << table[i][j] << ",";
+		    }
+		    cout << "\n";
+		}
+	    }
+
+	    vector<double> calcEquation2(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+	//       more like fill the table  
+		vector<double> results;
+	//         start fill the table
+		unordered_map<string, int> row;
+		int tableIndex = 0;
+		for(int i = 0; i < equations.size(); i++){
+		    if(row.find(equations[i][0]) == row.end() )
+			row[equations[i][0]] = tableIndex++;
+		    if(row.find(equations[i][1]) == row.end() )
+			row[equations[i][1]] = tableIndex++;
+		}
+		int size = row.size();
+		vector<vector<double>> table(size, vector<double>(size, -1));
+
+		for(int i = 0; i < equations.size(); i++){
+		    int i0 = row[equations[i][0]];
+		    int i1 = row[equations[i][1]];
+			table[i0][i1] = values[i];
+			table[i1][i0] = 1.0 / values[i];
+		}
+		// printTable(table);
+	//         fill them now
+		bool isChanged = true;
+		while(isChanged){
+		    isChanged = false;
+		    for(int i = 0; i < size; i++){            
+			isChanged = isChanged || fill(table, i);
+		    }    
+		}
+		// printTable(table);
+		for(int i = 0; i < queries.size(); i++){
+		    if(row.find(queries[i][0]) != row.end()
+		      && row.find(queries[i][1]) != row.end()){
+			int i0 = row[queries[i][0]];
+			int i1 = row[queries[i][1]];
+			results.push_back(i0 == i1 ? 1 : (i0 < i1 ? table[i0][i1] : 1 / table[i1][i0]) );
+		    }
+		    else{
+			results.push_back(-1);
+		    }
+		}
+		return results;
+	    }
+
+	    bool fill(vector<vector<double>> & table, int row){
+		bool isChanged = false;
+		for(int i = 0; i < row; i++){
+		    if(table[i][row] != -1){
+			for(int j = i+1; j < table.size(); j++){
+			    if(table[i][j] != -1){
+				if(table[row][j] == -1){
+				    table[row][j] = 1 / table[i][row] * table[i][j];                        
+				    table[j][row] = 1 / table[row][j]; 
+				    isChanged = true;    
+				}                        
+			    }                    
+			}                    
+		    }                    
+		}
+		for(int i = row+1; i < table.size(); i++){
+		    if(table[row][i] != -1){
+			isChanged = isChanged || fill(table, i);
+			for(int j = 0; j < table.size(); j++){
+			    if(table[i][j] != -1){
+				if(table[row][j] == -1){
+				    isChanged = true;
+				    table[row][j] = table[row][i] * table[i][j];                         
+				    table[j][row] = 1 / table[row][j]; 
+				}                        
+			    }                    
+			}
+		    }
+		}
+		return isChanged;
+	    }
+
+	    unordered_map<string,unordered_map<string,double>>mp;
+	    unordered_set<string>visited;
+
+	    double dfs(string src, string dst, double p)
+	    {
+		if(mp.find(src)==mp.end() or mp.find(dst)==mp.end()) return -1;
+		if(src==dst) return p;
+		double k = -1;
+		visited.insert(src);
+		for(auto x:mp[src])
+		{
+		    if(visited.find(x.first) == visited.end())
+			k = dfs(x.first,dst,p*x.second);
+		    if(k != -1) return k;
+		}
+		return -1.0;
+	    }
+
+	    vector<double> calcEquation(vector<vector<string>>& eq, vector<double>& val, vector<vector<string>>& q) {
+		vector<double>res;
+		for(int i=0 ; i<eq.size() ; i++)
+		{
+		    mp[eq[i][0]][eq[i][1]] = val[i];
+		    mp[eq[i][1]][eq[i][0]] = 1/val[i];
+		}
+		for(int i=0 ; i<q.size() ; i++)
+		{
+		    visited.clear();
+		    double k = dfs(q[i][0],q[i][1],1.0);
+		    res.push_back(k);
+		}
+		return res;
+	    }
 };
