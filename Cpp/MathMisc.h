cb986793720dce965552d004a27febf27b242b91
Zhenyi He <snowymohehe@gmail.com>
2020-10-08 14:14:27 -0400

    Finish No.794 TicTacToe state

--- Cpp/MathMisc.h
+++ Cpp/MathMisc.h
@@ -106,4 +106,151 @@ class MathMisc {
 		}
 		return false;
 	}
+	
+// 	Mine No.794[M] Valid Tic-Tac-Toe
+// 	A Tic-Tac-Toe board is given as a string array board. Return True if and only if it is possible to reach this board position during the course of a valid tic-tac-toe game.
+
+// The board is a 3 x 3 array, and consists of characters " ", "X", and "O".  The " " character represents an empty square.
+
+// Here are the rules of Tic-Tac-Toe:
+
+// Players take turns placing characters into empty squares (" ").
+// The first player always places "X" characters, while the second player always places "O" characters.
+// "X" and "O" characters are always placed into empty squares, never filled ones.
+// The game ends when there are 3 of the same (non-empty) character filling any row, column, or diagonal.
+// The game also ends if all squares are non-empty.
+// No more moves can be played if the game is over.
+// Example 1:
+// Input: board = ["O  ", "   ", "   "]
+// Output: false
+// Explanation: The first player always plays "X".
+
+// Example 2:
+// Input: board = ["XOX", " X ", "   "]
+// Output: false
+// Explanation: Players take turns making moves.
+
+// Example 3:
+// Input: board = ["XXX", "   ", "OOO"]
+// Output: false
+
+// Example 4:
+// Input: board = ["XOX", "O O", "XOX"]
+// Output: true
+	bool validTicTacToe(vector<string>& board) {
+		int numX = 0;        int numO = 0;
+		unordered_map<int,int> countRowX, countRowO, countColX, countColO, countDiaX, countDiaO, winX, winO; // first index is 0-r 1-c 2-d0 3-d2
+		for(int i = 0; i < board.size(); i++){
+		   for(int j = 0; j < board[i].length(); j++){
+		       if(board[i][j] == 'X'){
+			   ++numX;  
+			   countRowX[i]+=1;
+			   countColX[j]+=1;
+			   if(i == j)
+				countDiaX[0]+=1;
+			   if(i + j == 2){
+			       countDiaX[2] += 1;
+			   }                       
+
+			   if(countRowX[i] == 3){
+			       if(winX.count(0) > 0){
+				   return false;
+			       }
+			       winX[0] = i;
+			   }
+			   if(countColX[j] == 3){
+			       if(winX.count(1) > 0){
+				   return false;
+			       }
+			       winX[1] = j;
+			   }
+			   if(i == j && countDiaX[0] == 3){
+			       if(winX.count(2) > 0){
+				   return false;
+			       }
+			       winX[2] = 0;
+			   }
+			   if(i + j == 2 && countDiaX[2] == 3){
+			       if(winX.count(3) > 0){
+				   return false;
+			       }
+			       winX[3] = 2;
+			   }
+		       }else if(board[i][j] == 'O'){
+			   ++ numO;
+			   countRowO[i]+=1;
+			   countColO[j]+=1;
+			   if(i == j)
+				countDiaO[0]+=1;
+			   if(i + j == 2)
+			       countDiaO[2] += 1;
+
+			   if(countRowO[i] == 3){
+			       if(winO.count(0) > 0){
+				   return false;
+			       }
+			       winO[0] = i;
+			   }
+			   if(countColO[j] == 3){
+			       if(winO.count(1) > 0){
+				   return false;
+			       }
+			       winO[1] = j;
+			   }
+			   if(countDiaO[0] == 3){
+			       if(winO.count(2) > 0){
+				   return false;
+			       }
+			       winO[2] = 0;
+			   }
+			   if(countDiaO[2] == 3){
+			       if(winO.count(3) > 0){
+				   return false;
+			       }
+			       winO[3] = 2;
+			   }
+		       }
+		   }
+		}
+		// cout << "rule 1 \n";
+		if(numX < numO || numX > numO+1)
+		    return false;
+	//         rule 2, if both win
+		// cout << "rule 2 \n";
+		if(numX > numO){
+	//             at most X win
+		    if(winO.count(0)+winO.count(1)+winO.count(2)+winO.count(3) > 0)
+			return false;
+		}
+		if(numX == numO){
+		    if(winX.count(0)+winX.count(1)+winX.count(2)+winX.count(3) > 0)
+			return false;
+		}
+		if((winX.count(0)+winX.count(1)+winX.count(2)+winX.count(3) > 0)
+		   && (winO.count(0)+winO.count(1)+winO.count(2)+winO.count(3) > 0)){
+		       return false;
+		    }
+	//             rule 2.2 if one win requires more than one move
+	//             aka both row and col win is fine but
+	//             if dia is win with row and col, then the row or col should share the same cell
+		    if(winX.count(0)+winX.count(1)+winX.count(2) == 3){
+			if(winX[0] != 0 || winX[1] != 0)
+			    return false;
+		    }
+		    if(winX.count(0)+winX.count(1)+winX.count(3) == 3){
+			if(winX[0] != 0 || winX[1] != 2)
+			    return false;
+		    }
+		    if(winO.count(0)+winO.count(1)+winO.count(2) == 3){
+			if(winO[0] != 0 || winO[1] != 0)
+			    return false;
+		    }
+		    if(winO.count(0)+winO.count(1)+winO.count(3) == 3){
+			if(winO[0] != 0 || winO[1] != 2)
+			    return false;
+		    }
+	//         rule 3 only the one who makes the last move can win
+		return true;
+	    }
+	
 };
