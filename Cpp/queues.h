5e00364f8804ef33ac6cf3bfd1c860e72137b7dd
Zhenyi He <snowymohehe@gmail.com>
2020-10-09 00:31:20 -0400

    Finish No.378 [M] Find Minimum in Rotated Sorted Array

--- Cpp/queues.h
+++ Cpp/queues.h
@@ -55,4 +55,74 @@ namespace Queues {
 
 	// 225. Implement Stack using Queues
 	// See Python
+	
+// 	Mine No.378 [M] Find Minimum in Rotated Sorted Array
+// 	Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+
+// Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+// Example:
+
+// matrix = [
+//    [ 1,  5,  9],
+//    [10, 11, 13],
+//    [12, 13, 15]
+// ],
+// k = 8,
+
+// return 13.
+// Note:
+// You may assume k is always valid, 1 ≤ k ≤ n2.
+	int kthSmallest(vector<vector<int>>& matrix, int k) {
+		int len = matrix.size();
+		bool small = true;
+		if(k > len*len/2){
+		    k = len*len-k+1;
+		    small = false;
+		}
+		if(small){
+		    priority_queue<int> potential;
+		    for(int i = 0, j = len-1; i < len && j >= 0;){
+			if((i+1) * (j+1) > k){
+			    --j;
+			    // cout << "move to left " << j << "\n";
+			}else{
+			    // move to next row
+			    for(int rowi = 0; rowi <=j; rowi++){
+				// cout << "add " << matrix[i][rowi] << "\n";
+				potential.push(matrix[i][rowi]);
+				if(potential.size() > k){
+				    potential.pop();
+				}
+			    }
+
+			    ++i;
+			    // cout << "move down " << i << "\n";
+			}
+		    }   
+		    return potential.top();
+		}else{
+		    priority_queue<int, vector<int>, greater<int>> potential;
+		    cout << "find the " << k << "th from the end\n";
+		    for(int i = 0, j = len-1; i < len && j >= 0;){
+			cout << "cal " << len-i << "*" << len-j << "\n";
+			if((len-i) * (len-j) <= k){
+			    // move to left
+			    for(int coli = i; coli < len; coli++){
+				// cout << "add " << matrix[coli][j] << "\n";
+				potential.push(matrix[coli][j]);
+				if(potential.size() > k){
+				    potential.pop();
+				}
+			    }                    
+			    --j;
+			    // cout << "move to left " << j << "\n";
+			}else{
+			    ++i;
+			    // cout << "move down " << i << "\n";
+			}
+		    }
+		    return potential.top();
+		}
+	    }
 };
