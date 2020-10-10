0d5a8a97bf902aa8ef30c547dfaf67d5f3eac494
Zhenyi He <snowymohehe@gmail.com>
2020-10-10 02:18:51 -0400

    Finish No.938 Range Sum of BST

--- Cpp/binarysearchtrees.h
+++ Cpp/binarysearchtrees.h
@@ -272,6 +272,33 @@ public:
 			return left ? left : root;
 		}
 	}
+	
+	// Mine No.938 [E] Range Sum of BST
+// 	Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+// The binary search tree is guaranteed to have unique values.
+// Example 1:
+
+// Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+// Output: 32
+// Example 2:
+
+// Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+// Output: 23
+	int rangeSumBST(TreeNode* root, int L, int R) {
+		if(root == NULL)
+		    return 0;
+		int ret = 0;
+		if(root->val <= R && root->val >= L){
+		    ret += root->val;
+		}
+		if(root->val > L){
+		    ret += rangeSumBST(root->left, L, R);
+		}
+		if(root->val < R){
+		    ret += rangeSumBST(root->right, L, R);
+		}
+		return ret;
+	    }
 };
 
 // 173. Binary Search Tree Iterator
