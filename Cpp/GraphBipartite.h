ff4233eb8bad6e97d8d4f5c0da739196c0d314d1
Zhenyi He <snowymohehe@gmail.com>
2020-10-11 03:50:09 -0400

    Finish No.785 Is Graph Bipartite

--- Cpp/GraphBipartite.h
+++ Cpp/GraphBipartite.h
@@ -27,4 +27,43 @@ namespace GraphBiparite {
 		}
 		return true;
 	}
+	// Mine (DFS) No.785 [M] Is Graph Bipartite
+	unordered_map<int,int> group;
+	    bool isBipartite(vector<vector<int>>& graph) {
+		if(graph.size() == 0)
+		    return true;
+		for(int i = 0; i < graph.size(); i++){
+		    bool curRet = isBipartite(graph, i);
+		    if(!curRet)
+			return false;
+		}
+		return true;
+	    }
+	    bool isBipartite(vector<vector<int>>& graph, int vert){
+		// cout << "check vert " << vert << "\n";
+		if(group.count(vert) == 0){
+		    group[vert] = vert+1;
+		}
+		int groupIdx = group[vert];
+		// cout << "cur group " << groupIdx << "\n";
+
+		for(int j = 0; j < graph[vert].size(); j++){
+		    if(group.count(graph[vert][j]) > 0){
+			// if they are the same, return false
+			if(group[graph[vert][j]] == groupIdx){
+			    // cout << "same group " << vert << "-" << graph[vert][j] << "\n";
+			    return false;
+			}                    
+			// group[graph[i][j]] should be in the same group groupIdx
+			// group[graph[vert][j]] = -groupIdx;
+		    }
+		    else{
+			group[graph[vert][j]] = -groupIdx;
+			bool curRet = isBipartite(graph, graph[vert][j]);
+			if(!curRet)
+			    return false;
+		    }
+		}
+		return true;
+	    }
 }
